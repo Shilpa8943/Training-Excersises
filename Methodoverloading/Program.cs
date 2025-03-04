b84793a7a3a032388ffe6e460699eacf9f359383
Shilpa8943 <trshilpa14@gmail.com>
2025-03-04 11:14:54 +0530

    Create Program.cs

---
+++ Methodoverloading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodoverloading
+{
+    class Program
+    {
+
+        static int add(int x,int y)
+        {
+            return x + y;
+        }
+
+        static int add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+
+        static double add(double x, double y)
+        {
+            return x + y;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("sum of two integer numbers = " + add(3, 5));
+            Console.WriteLine("sum of three integer numbers = " + add(3, 5,8));
+            Console.WriteLine("sum of two floating point numbers = " + add(3.2, 5.4));
+
+
+        }
+    }
+}
